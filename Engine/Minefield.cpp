0513f5dbdab8000eb9639a971c834f9a6ad7901e
The-Lab-Exerpiment <akhilmudiyala@gmail.com>
2025-06-13 12:16:18 +0100

    draw minefield (wip)

--- Engine/Minefield.cpp
+++ Engine/Minefield.cpp
@@ -1,24 +1,37 @@
 #include "Minefield.h"
 
-Minefield::Minefield(const int _mines) :
+Minefield::Minefield(const int _mines, Graphics& _gfx) :
 	rng(std::random_device()()),
-	fieldRange(0, width* height - 1) {
-	//do smth
+	fieldRange(0, width* height - 1),
+	gfx(_gfx) {
+	mines = _mines;
+}
+
+void Minefield::Draw() {
+	for (int tile = 0;tile < width * height;tile++) {
+		if (tiles[tile].IsMine()) {
+			for (int x = 0;x < size;x++) {
+				for (int y = 0;y < size;y++) {
+					gfx.PutPixel(tile % width * size + x, tile / width * size + y, Color(255, 0, 0));
+				}
+			}
+		}
+	}
 }
 
 void Minefield::SpawnMine() {
-	int bombTile;
+	int mineTile;
 	do {
-		bombTile = fieldRange(rng);
-	} while (!tiles[bombTile].IsBomb());
-	tiles[bombTile].SetBomb();
+		mineTile = fieldRange(rng);
+	} while (!tiles[mineTile].IsMine());
+	tiles[mineTile].SetMine();
 
 }
 
-void Minefield::Tile::SetBomb() {
-	isBomb = true;
+void Minefield::Tile::SetMine() {
+	isMine = true;
 }
 
-bool Minefield::Tile::IsBomb() const {
-	return isBomb;
+bool Minefield::Tile::IsMine() const {
+	return isMine;
 }
--- Engine/Minefield.h
+++ Engine/Minefield.h
@@ -1,10 +1,12 @@
 #pragma once
 #include <random>
+#include "Graphics.h"
 
 class Minefield {
 public:
-	Minefield(const int _mines);
+	Minefield(const int _mines, Graphics& _gfx);
 	void SpawnMine();
+	void Draw();
 
 private:
 	class Tile {
@@ -14,16 +16,22 @@ private:
 			Flagged,
 			Revealed
 		};
-		bool IsBomb() const;
-		void SetBomb();
+		bool IsMine() const;
+		void SetMine();
 
 	private:
 		State state = State::Hidden;
-		bool isBomb = false;
+		bool isMine = false;
 	};
 
 	std::mt19937 rng;
 	std::uniform_int_distribution<int> fieldRange;
+
+	Graphics& gfx;
+
+	int mines;
+
+	static constexpr int size = 10;
 
 	static constexpr int width = 15;
 	static constexpr int height = 10;
